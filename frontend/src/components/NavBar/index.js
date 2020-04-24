82f398ce485fa1a293f0338ca9ba1b2e8400acf8
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-04-24 00:29:19 -0300

    agrego estructura de navbar

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -1,5 +1,45 @@
 import React from 'react';
 
 export default function NavBar() {
-  return <div>asdsad</div>;
+  return (
+    <nav className="nav ">
+      <div className="nav__contenedor contenedor">
+        <div className="logo">
+          <p>Logo</p>
+
+          <div className="menu-movil">
+            <i className="fas fa-bars" />
+          </div>
+        </div>
+        <ul className="menu">
+          <li className=" menu__item">
+            <a href="#" className="menu__link menu__link-h">
+              Home
+            </a>
+          </li>
+          <li className="menu__item">
+            <a href="#" className="menu__link">
+              Anillos
+            </a>
+          </li>
+          <li className="menu__item">
+            <a href="#" className="menu__link">
+              Relojes
+            </a>
+          </li>
+          <li className="menu__item">
+            <a href="#" className="menu__link">
+              Accesorios
+            </a>
+          </li>
+
+          <li className="menu__item">
+            <a href="#" className="menu__link">
+              Contacto
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
