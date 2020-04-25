1358e39f3bb368ba06a35e47d2ae4a7372e8d63e
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-04-24 23:50:22 -0300

    agrego hero

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 import NavBar from '../NavBar';
+import Hero from '../Hero';
 
 import styles from './index.module.scss';
 
 function App() {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <Hero />
+    </>
+  );
 }
 
 export default App;
