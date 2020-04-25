b05b60dbd5032e8ed75c3699bf016fe8270c1958
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-04-25 00:00:12 -0300

    agrego estructura y estilos de hero

--- frontend/src/components/Hero/index.js
+++ frontend/src/components/Hero/index.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+
+import styles from './index.module.scss';
 
 export default function Hero() {
   return (
-    <div>
-      Hero
+    <div className={styles.hero}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Joyeria gold</h1>
+          <p>Lorem ipsum t amet consectetur adipisicinelit amet consectetur adipisicinelit</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
