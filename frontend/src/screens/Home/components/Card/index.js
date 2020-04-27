2b10fa3d05e548b10e948c5f174ed5ea0c990ff3
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-04-27 01:36:22 -0300

    agrego fontawesome y constante con los datos para cada card

--- frontend/src/screens/Home/components/Card/index.js
+++ frontend/src/screens/Home/components/Card/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCommentDots, faGem } from '@fortawesome/free-solid-svg-icons';
+import { faCircle } from '@fortawesome/free-regular-svg-icons';
+
 import styles from './index.module.scss';
-export default function Card({urlImg}) {
+
+export default function Card({ urlImg, icon, title, description }) {
   return (
-    <div className={styles.card}>
-      <div className={styles.containerImg}>
-        <img src={urlImg} alt="" />
+    <div className={icon ? styles.cardIcon : styles.card}>
+      <div className={icon ? styles.containerIcon : styles.containerImg}>
+        {icon ? <FontAwesomeIcon icon={faCircle} /> : <img src={urlImg} alt="" />}
       </div>
       <div className={styles.containerDesc}>
-        <h3 className={styles.title}>Anillos</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minimaquibusdam numquam natus voluptates
-          consectetur, nisi optio impedit atque eius nam incidunt molestiae doloremque harum. Deleniti nemo
-          non eaque veritatis aperiam?
-        </p>
+        <h3 className={styles.title}>{title}</h3>
+        <p>{description}</p>
       </div>
     </div>
   );
--- frontend/src/screens/Home/components/WhatWeDo/index.js
+++ frontend/src/screens/Home/components/WhatWeDo/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 import Title from '../../../../components/Title';
 import Card from '../Card';
+
+import { CARDS } from './constants/cards';
 import styles from './index.module.scss';
 
-export default function WhatWeDo({ urlImg }) {
+export default function WhatWeDo() {
   return (
     <div className={styles.whatWeDo}>
       <Title title="Lo que hacemos" big />
       <div className={styles.container}>
-       <Card urlImg={urlImg} />       
-       <Card urlImg={urlImg} />       
-       <Card urlImg={urlImg} />       
-
+        {CARDS.map(card => (
+          <Card key={card.key} {...card} />
+        ))}
       </div>
     </div>
   );
