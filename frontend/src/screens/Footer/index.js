fe059e98193287f3bfd059bcf1978e1fd3253f02
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-04-28 01:06:51 -0300

    agrego estructura de newsletter

--- frontend/src/screens/Footer/index.js
+++ frontend/src/screens/Footer/index.js
@@ -7,18 +7,22 @@ import styles from './index.module.scss';
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <div>
-        <h4>Newsletter</h4>
-        <p>asdsadasdsadas</p>
-        <p>asdsadasdsadas</p>
-        <p>asdsadasdsadas</p>
+      <div className={styles.box}>
+        <h4>NEWSLETTER</h4>
+        <p>
+          No te pierdas todas nuestras novedades y los mejores descuentos
+          <form>
+            <input placeholder="Email..." type="text" />
+            <input type="submit" value="Enviar" />
+          </form>
+        </p>
       </div>
-    
-      <div className="">
+
+      <div className={styles.box}>
         <h4>Menu</h4>
         <Menu column />
       </div>
-      <div className="">
+      <div className={styles.box}>
         <h4>Redes</h4>
         <Menu column socialNetwork />
       </div>
