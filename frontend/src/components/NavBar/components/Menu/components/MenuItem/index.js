164090f571db79dc372d34b764fa0085ef2d581b
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-04-28 02:22:35 -0300

    agrego estilos al nnewsletter

--- frontend/src/components/NavBar/components/Menu/components/MenuItem/index.js
+++ frontend/src/components/NavBar/components/Menu/components/MenuItem/index.js
@@ -6,8 +6,8 @@ import styles from './index.module.scss';
 export default function MenuItem({ textItem, icon, urlProfile, column }) {
   return (
     <li className={column ? styles.itemMenuColumn : styles.itemMenu}>
-      <a href={icon ? urlProfile : '#null'} className={styles.linkMenu}>
-        {icon && <FontAwesomeIcon icon={icon} />} {textItem}
+      <a target={icon && 'blank'} href={icon ? urlProfile : '#null'} className={styles.linkMenu}>
+        {icon && <FontAwesomeIcon className={styles.icon} icon={icon} />} {textItem}
       </a>
     </li>
   );
--- frontend/src/screens/Footer/components/Newsletter/index.js
+++ frontend/src/screens/Footer/components/Newsletter/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
+import styles from './index.module.scss';
+
 export default function Newsletter() {
   return (
     <>
       <div>
-        <p> No te pierdas todas nuestras novedades y los mejores descuentos</p>
-        <form action="">
-          <input placeholder="Email..." type="text" />
-          <input type="submit" value="Enviar" />
+        <p className={styles.subtitle}> No te pierdas todas nuestras novedades y los mejores descuentos</p>
+        <form className={styles.form} action="">
+          <input className={styles.inputText} placeholder="Email..." type="text" />
+          <input className={styles.inputSubmit} type="submit" value="Enviar" />
         </form>
       </div>
     </>
