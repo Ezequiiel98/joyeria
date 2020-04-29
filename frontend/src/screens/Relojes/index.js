56a583fffd390f45789af4a09494afae9feac5b3
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-04-29 03:29:51 -0300

    agrego estuctura de screen relojes

--- frontend/src/screens/Relojes/index.js
+++ frontend/src/screens/Relojes/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
+import Title from '../../components/Title';
+
+import Reloj from './assets/img/reloj-11.jpg';
 import styles from './index.module.scss';
 
 export default function Relojes() {
-  return <p>Relojes</p>;
+  return (
+    <>
+      <Title tile="Relojes" />
+      <div className={styles.container}>
+        <div className={styles.product}>
+          <div className={styles.imgProduct}>
+            <img src={Reloj} alt="" />
+          </div>
+          <div className={styles.description}>
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
