d108e4f21a5e383d322ad03e4c206810dfa22e81
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-05-03 22:44:42 -0300

    agrego componente alhajas y sus imagenes

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import NavBar from '../NavBar';
-import Relojes from '../../screens/Relojes';
+import Alhajas from '../../screens/Alhajas';
 
 import styles from './index.module.scss';
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <NavBar />
-      <Relojes />
+      <Alhajas />
     </>
   );
 }
---
+++ frontend/src/screens/Alhajas/constants/alhajas.js
@@ -0,0 +1,39 @@
+import alhaja0 from '../assets/img/alhajas/alhaja-1.jpg';
+import alhaja1 from '../assets/img/alhajas/alhaja-2.jpg';
+import alhaja2 from '../assets/img/alhajas/alhaja-3.jpg';
+import alhaja3 from '../assets/img/alhajas/alhaja-4.jpg';
+
+export const ALHAJAS = [
+  {
+    id: 0,
+    title: 'alhaja 0',
+    description: 'n',
+    imgURL: alhaja0,
+    stars: 2,
+    price: '$59'
+  },
+  {
+    id: 1,
+    title: 'alhaja 1',
+    description: 'n',
+    imgURL: alhaja1,
+    stars: 2,
+    price: '$159'
+  },
+  {
+    id: 2,
+    title: 'alhaja 2',
+    description: 'n',
+    imgURL: alhaja2,
+    stars: 5,
+    price: '$19'
+  },
+  {
+    id: 0,
+    title: 'alhaja 3',
+    description: 'a',
+    imgURL: alhaja3,
+    stars: 3,
+    price: '$79'
+  }
+];
