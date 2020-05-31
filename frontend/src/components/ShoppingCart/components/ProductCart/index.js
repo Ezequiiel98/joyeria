c85d87ccf1933c12f15fe23199c94341d18c1a37
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-05-31 01:54:15 -0300

    creo componente ProductCart

---
+++ frontend/src/components/ShoppingCart/components/ProductCart/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
+
+import styles from './index.module.scss';
+
+export default function ProductCart({ id, uuid, imgURL, title, price, category, onClick }) {
+  const product = { id, uuid, imgURL, title, price, category };
+
+  return (
+    <div className={styles.product}>
+      <div className={styles.containerImg}>
+        <img className={styles.img} src={imgURL} />
+      </div>
+      <span className={styles.title}>{title}</span>
+      <span className={styles.price}>${price}</span>
+      <button className={styles.buttonDelete} type="button" onClick={e => onClick(e, product)}>
+        <FontAwesomeIcon className={styles.icon} icon={faTrashAlt} />
+      </button>
+    </div>
+  );
+}
--- frontend/src/components/ShoppingCart/index.js
+++ frontend/src/components/ShoppingCart/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
 import { connect } from 'react-redux';
 
 import { deleteProductToCart } from '../../redux/actions';
 
+import ProductCart from './components/ProductCart';
 import styles from './index.module.scss';
 
 function ShoppingCart({ cart, deleteProductToCart }) {
@@ -16,16 +15,7 @@ function ShoppingCart({ cart, deleteProductToCart }) {
   return (
     <div className={styles.shoppingCart}>
       {cart.map(product => (
-        <div key={product.uuid} className={styles.product}>
-          <div className={styles.containerImg}>
-            <img className={styles.img} src={product.imgURL} />
-          </div>
-          <span>{product.title}</span>
-          <span className="text-right">${product.price}</span>
-          <button className={styles.buttonDelete} type="button" onClick={e => handleClick(e, product)}>
-            <FontAwesomeIcon className={styles.icon} icon={faTrashAlt} />
-          </button>
-        </div>
+        <ProductCart key={product.uuid} {...product} onClick={handleClick} />
       ))}
       <div className={styles.totalPrice}>
         <span>Total: ${cart.reduce((sum, product) => sum + product.price, 0)}</span>
