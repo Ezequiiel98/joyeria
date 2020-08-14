23885c55fef052ae7449a854b2ff93561507a265
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-08-14 02:35:06 -0300

    agrego proptypes al componente Title

--- frontend/src/components/Title/index.js
+++ frontend/src/components/Title/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './index.module.scss';
 
 export default function Title({ title, big }) {
   return big ? <h2 className={styles.title}>{title}</h2> : <h3 className={styles.title}>{title}</h3>;
+}
+
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
+  big: PropTypes.bool
 }
