ccc62a3ea26e6e73c08baf3bc19f11b26a9fac03
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-08-20 17:26:51 -0300

    borro el head de app

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -3,12 +3,10 @@ import React from 'react';
 import NavBar from '../NavBar';
 import Main from '../Main';
 import Footer from '../Footer';
-import Head from '../Head';
 
 function App() {
   return (
     <>
-      <Head />
       <NavBar />
       <Main />
       <Footer />
