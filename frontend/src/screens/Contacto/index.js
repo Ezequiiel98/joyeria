806f7c3932da7074f539aa73b6e26136b38ac04d
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-08-20 17:18:20 -0300

    agrego head a la screen contacto

--- frontend/src/screens/Contacto/index.js
+++ frontend/src/screens/Contacto/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Head from 'components/Head';
+
 import Map from './components/Map';
 import Field from './components/Field';
 import Button from './components/Button';
@@ -7,21 +9,26 @@ import styles from './index.module.scss';
 
 export default function Contacto() {
   return (
-    <div className={styles.contacto}>
-      <form className={styles.form} onSubmit={e => e.preventDefault()}>
-        <div className={styles.direction}>
-          <p>Buenos Aires </p>
-          <p> 45 789-635 </p>
-          <p> info@joyeria.com </p>
-        </div>
-        <Field type="text" label="Nombre" name="name" placeHolder="Nombre..." />
-        <Field type="number" label="Telefono" name="phone" placeHolder="Telefono..." />
-        <Field type="email" label="Email" name="email" placeHolder="Email..." />
-        <Field type="text" textarea label="Consulta" name="name" placeHolder="Su consulta..." />
-        <Button text="Enviar" />
-      </form>
+    <>
+      <Head title="Contacto">
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" />
+      </Head>
+      <div className={styles.contacto}>
+        <form className={styles.form} onSubmit={e => e.preventDefault()}>
+          <div className={styles.direction}>
+            <p>Buenos Aires </p>
+            <p> 45 789-635 </p>
+            <p> info@joyeria.com </p>
+          </div>
+          <Field type="text" label="Nombre" name="name" placeHolder="Nombre..." />
+          <Field type="number" label="Telefono" name="phone" placeHolder="Telefono..." />
+          <Field type="email" label="Email" name="email" placeHolder="Email..." />
+          <Field type="text" textarea label="Consulta" name="name" placeHolder="Su consulta..." />
+          <Button text="Enviar" />
+        </form>
 
-      <Map />
-    </div>
+        <Map />
+      </div>
+    </>
   );
 }
