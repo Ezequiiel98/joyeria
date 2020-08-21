4eed894eeb8f572b61f45b1039eb53630a3a6f16
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-08-21 00:48:54 -0300

    agrego modificaciones en el hero

--- frontend/src/components/Hero/index.js
+++ frontend/src/components/Hero/index.js
@@ -8,7 +8,7 @@ import styles from './index.module.scss';
 export default function Hero({ image, title, description, reverse }) {
   return (
     <div className={styles.hero} >
-      <Imagen src={image} big />
+      <Imagen className={styles.imgHero} src={image} alt={title} big />
       <div className={reverse ? styles.containerReverse : styles.container}>
         <div className={reverse ? styles.contentReverse : styles.content}>
           <h1 className={reverse ? styles.titleReverse : styles.title}>{title}</h1>
